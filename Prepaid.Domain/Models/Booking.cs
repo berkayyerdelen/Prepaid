37dff119e67af488da1bdf285d038708cd6d320c
Berkay <berkayyerdelen@gmail.com>
2024-07-04 23:27:58 +0200

    added payment information as value object

--- Prepaid.Domain/Models/Booking.cs
+++ Prepaid.Domain/Models/Booking.cs
@@ -29,8 +29,7 @@ public class Booking
         }
         UserId = userId;
     }
-
-
+    
     public void SetAccessSlot(AccessSlot accessSlot)
     {
         AccessSlot = accessSlot;
@@ -44,4 +43,22 @@ public class Booking
     {
         _bookingState = bookingState;
     }
+}
+
+public class PaymentInformation
+{
+    public PaymentInformation(string? paymentId, string? paymentToken, decimal amount, decimal serviceFee, DateTime? paymentTime)
+    {
+        PaymentId = paymentId;
+        PaymentToken = paymentToken;
+        Amount = amount;
+        ServiceFee = serviceFee;
+        PaymentTime = paymentTime;
+    }
+
+    public string? PaymentId { get; private set; }
+    public string? PaymentToken { get; private set; }
+    public decimal Amount { get; private set; } = 0.0m;
+    public decimal ServiceFee { get; private set; } = 0.0m;
+    public DateTime? PaymentTime { get; private set; }
 }
